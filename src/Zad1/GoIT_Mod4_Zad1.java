e67e3135cfb9cd62f698c7d323ae5c9eb7f527b6
sashaporitskiy <31014377+sashaporitskiy@users.noreply.github.com>
2017-09-25 16:37:25 +0300

    вариант 2 повторное размещение в репозиторий

--- src/Zad1/GoIT_Mod4_Zad1.java
+++ src/Zad1/GoIT_Mod4_Zad1.java
@@ -1,4 +1,36 @@
 package Zad1;
-
+import java.util.Scanner;
+/**
+ * Задание 1
+ Написать функцию которая считает в консоли до числа Х.
+ Аргументы функции: число Х
+ Например Х = 5.
+ Вывод программы:
+ 1
+ 2
+ 3
+ 4
+ 5
+ */
 public class GoIT_Mod4_Zad1 {
+    public static void main(String[] args) {
+        int x= enterNumber();
+        outNumber(x );
+    }
+    public static int enterNumber(){
+        Scanner scr = new Scanner(System.in);
+        System.out.println("Enter integer number - > ");
+        int a = scr.nextInt();
+        if (a==0){
+            System.out.println("you entered a number zero, please enter a different integer number");
+            a = scr.nextInt();
+        }
+        return a;
+    }
+    static void outNumber(int x ){
+
+        for (int i=0;i<=x;i++){
+            System.out.println(i);
+        }
+    }
 }

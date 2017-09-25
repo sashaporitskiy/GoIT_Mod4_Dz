c44862e6806bd46bdd19a37e94bec17f44808f72
sashaporitskiy <31014377+sashaporitskiy@users.noreply.github.com>
2017-09-25 18:23:14 +0300

    выполнил задание 6

--- src/Zad6/GoIT_Mod4_Zad6.java
+++ src/Zad6/GoIT_Mod4_Zad6.java
@@ -1,4 +1,40 @@
 package Zad6;
 
+import java.util.Scanner;
+
+/**
+ *Задание 5
+ Решить задачу 1, без использования циклов. Используя рекурсию.
+ *
+ * Задание 2
+ Написать функцию drawRectangle которая рисует в консоли прямоугольник из символов '+'
+ Аргументы функции: ширина прямоугольника в символах, высота прямоугольника в символах
+
+ Например 3 на 2
+ Вывод программы:
+ + + +
+ + + +
+
+ */
 public class GoIT_Mod4_Zad6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter integer number - >  ");
+        int a = sc.nextInt();
+        System.out.println("Enter integer number - >  ");
+        int b = sc.nextInt();
+        System.out.println("Result:");
+        drawRectangle(a,a,b);
+    }
+    
+    static void drawRectangle(int ta, int a, int b) {
+        if (ta > 0) {
+            System.out.print("+ ");
+            drawRectangle(ta - 1, a, b);
+        }
+        else if ((ta == 0) && (b > 1)) {
+            System.out.println("");
+            drawRectangle(a, a, b - 1);
+        }
+    }
 }
